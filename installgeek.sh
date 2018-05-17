62d60ad77ca241f6f90e88ed9bf7c2b757d42117
thandonguocmo2020 <thandonguocmo2020@users.noreply.github.com>
2018-05-17 22:43:17 +0700

    Update installgeek.sh

--- installgeek.sh
+++ installgeek.sh
@@ -7,7 +7,5 @@ echo "`sudo cd /var`"
 echo "`sudo mkdir geek`"
 echo "`sudo cd geek`"
 echo "`sudo wget https://github.com/GeekCash/cpuminer-multi/files/1957549/cpuminer-multi.zip`"
-echo "`sudo unzip cpuminer-multi.zip`"
-echo "`sudo cd cpuminer-multi/`"
-echo "`sudo nano cpuminer-conf.json`"
+
 
